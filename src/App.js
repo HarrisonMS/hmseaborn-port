3975601ea7981be4059ec0884e639d2e0e4dd6b6
Harrison <hmseaborn@gmail.com>
2021-02-19 08:21:01 -0800

    clean up

--- src/App.js
+++ src/App.js
@@ -6,25 +6,9 @@ import HomePage from "./pages/homePage/homePage";
 import Resume from "./components/resume/resume.component";
 
 function App() {
-	const [user, setUser] = useState(null);
-	useEffect(() => {
-		axios.get("https://api.github.com/users/harrisonMS/repos").then((user) => {
-			setUser(user);
-		});
-	}, []);
-	console.log(user);
 	return (
 		<div className="App">
 			<Route exact path="/" component={HomePage} />
-			{/* <Route exact path="/resume" component={Resume} /> */}
-			{/* <Route
-				path="/resume"
-				component={() => {
-					window.location.href =
-						"https://drive.google.com/file/d/1BLi4oYmla4yprZ_oVK9pZLFtWEgcIGmD/view?usp=sharing";
-					return null;
-				}} */}
-			/>
 		</div>
 	);
 }
